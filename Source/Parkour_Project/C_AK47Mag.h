fd9a3b734836c82aab88f7f146cd1d18d55d9f08
IbbyHussain <ibrahimhussain2002@gmail.com>
2020-02-09 11:51:43 -0800

    AK47 AMMO
    Created blueprint of mag
    added materials
    exposed its properties to blueprint

--- Source/Parkour_Project/C_AK47Mag.h
+++ Source/Parkour_Project/C_AK47Mag.h
@@ -21,6 +21,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, Category = "AK47")
 	USkeletalMeshComponent* MeshComponent;
 
 public:	
