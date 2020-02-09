b84010287e12a047552acbd678844f77420c43d4
IbbyHussain <ibrahimhussain2002@gmail.com>
2020-02-09 11:48:16 -0800

    AK47 AMMO
    Created new class
    added class mesh

---
+++ Source/Parkour_Project/C_AK47Mag.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_AK47Mag.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class PARKOUR_PROJECT_API AC_AK47Mag : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_AK47Mag();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	USkeletalMeshComponent* MeshComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
