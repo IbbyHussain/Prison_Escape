a32ce6b3aac16bf40326f18c397ab34a0248bbc5
Ibby <ibrahimhussain2002@gmail.com>
2020-02-14 07:52:31 -0800

    AK47 PIckup
    Creates class and adds mesh

---
+++ Source/Parkour_Project/C_AK47PickUp.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_AK47PickUp.generated.h"
+
+UCLASS()
+class PARKOUR_PROJECT_API AC_AK47PickUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_AK47PickUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "AK47")
+	USkeletalMeshComponent* MeshComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
