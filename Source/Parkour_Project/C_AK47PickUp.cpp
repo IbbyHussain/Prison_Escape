4d0abf1e6c6cadd7b2dc204005f454309292b34f
Ibby <ibrahimhussain2002@gmail.com>
2020-02-15 08:53:49 -0800

    ignore

--- Source/Parkour_Project/C_AK47PickUp.cpp
+++ Source/Parkour_Project/C_AK47PickUp.cpp
@@ -27,7 +27,6 @@ void AC_AK47PickUp::BeginPlay()
 	Super::BeginPlay();
 
 	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AC_AK47PickUp::OnOverlapBegin);
-	
 }
 
 void AC_AK47PickUp::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
--- Source/Parkour_Project/C_AK47PickUp.h
+++ Source/Parkour_Project/C_AK47PickUp.h
@@ -30,6 +30,7 @@ protected:
 	UFUNCTION(BlueprintCallable, Category = "OnOverlapBeginAK47PickUp")
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	
 
 public:	
 	// Called every frame
