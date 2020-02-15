f691d665bae169c453b290f1dad2de3503c32a74
Ibby <ibrahimhussain2002@gmail.com>
2020-02-15 04:04:22 -0800

    AK47 Pickup
    Rewrote all pick up code and added physics and fixed collision triggers

---
+++ Source/Parkour_Project/C_AK47PickUp.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_AK47PickUp.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AC_AK47PickUp::AC_AK47PickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	RootComponent = MeshComponent;
+	
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>("BoxComponent");
+	BoxComponent->AttachTo(RootComponent);
+
+	SetActorRelativeScale3D(FVector(0.15));
+}
+
+// Called when the game starts or when spawned
+void AC_AK47PickUp::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AC_AK47PickUp::OnOverlapBegin);
+	
+}
+
+void AC_AK47PickUp::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlapped"));
+}
+
+// Called every frame
+void AC_AK47PickUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MeshComponent->SetSimulatePhysics(true);
+}
+
