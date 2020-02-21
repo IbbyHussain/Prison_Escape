1319eca8fd31f75488946db719d6d9335dee4db2
Ibby <ibrahimhussain2002@gmail.com>
2020-02-21 11:33:26 -0800

    AI
    created a new ai controller and black board

---
+++ Source/Parkour_Project/C_SamuariAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "C_SamuariAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOUR_PROJECT_API AC_SamuariAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
