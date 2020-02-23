1020064a5fabce2af71ff372981af7c1f53daa9f
Ibby <ibrahimhussain2002@gmail.com>
2020-02-23 06:33:30 -0800

    AI
    Added ai searching and finding player location

--- Source/Parkour_Project/CTask_FindPlayerLocation.cpp
+++ Source/Parkour_Project/CTask_FindPlayerLocation.cpp
@@ -9,11 +9,14 @@
 #include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
 #include "C_SamuariAIController.h"
 #include "GameFramework/Character.h"
+#include "Player_Character.h"
 #include "BlackBoardKeys.h"
 
 //CONSTRUCTOR
 UCTask_FindPlayerLocation::UCTask_FindPlayerLocation(FObjectInitializer const& object_initializer)
 {
+	NodeName = TEXT("Find Player Location");
+
 	bIsSearchingRandomLocation = false;
 
 	SearchRadius = 150.0f;
@@ -22,5 +25,32 @@ UCTask_FindPlayerLocation::UCTask_FindPlayerLocation(FObjectInitializer const& o
 //TASK->find player's location
 EBTNodeResult::Type UCTask_FindPlayerLocation::ExecuteTask(UBehaviorTreeComponent & Owner, uint8 * node_memory)
 {
-	return EBTNodeResult::Type();
+	//Get player's character and AI's controller
+	ACharacter* const Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	auto const Controller = Cast<AC_SamuariAIController>(Owner.GetAIOwner());
+
+	//Get player location
+	FVector const PlayerLocation = Player->GetActorLocation();
+
+	if(bIsSearchingRandomLocation)
+	{
+		FNavLocation Location;
+
+		//Get navigation system and call a random location near player
+		UNavigationSystemV1* const NavigationSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+
+		if (NavigationSystem->GetRandomPointInNavigableRadius(PlayerLocation, SearchRadius, Location, nullptr))
+		{
+			Controller->get_blackboard()->SetValueAsVector(bb_Keys::TargetLocation, Location.Location);
+		}
+	}
+
+	else
+	{
+		Controller->get_blackboard()->SetValueAsVector(bb_Keys::TargetLocation, PlayerLocation);
+	}
+	
+	//Finish task
+	FinishLatentTask(Owner, EBTNodeResult::Succeeded);
+	return EBTNodeResult::Succeeded;
 }
