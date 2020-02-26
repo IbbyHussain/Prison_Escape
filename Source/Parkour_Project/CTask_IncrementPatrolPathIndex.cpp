2c24105be6b99d854aab74b40f6469c6c047b534
Ibby <ibrahimhussain2002@gmail.com>
2020-02-26 10:31:27 -0800

    AI
    Created new task for incrementing the patrol path index and created an constructor

---
+++ Source/Parkour_Project/CTask_IncrementPatrolPathIndex.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CTask_IncrementPatrolPathIndex.h"
+
+
+UCTask_IncrementPatrolPathIndex::UCTask_IncrementPatrolPathIndex(FObjectInitializer const& ObjectInitializer)
+{
+
+}
+
+
+EBTNodeResult::Type UCTask_IncrementPatrolPathIndex::ExecuteTask(UBehaviorTreeComponent & Owner, uint8 * NodeMemory)
+{
+	return EBTNodeResult::Type();
+}
---
+++ Source/Parkour_Project/CTask_IncrementPatrolPathIndex.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "CTask_IncrementPatrolPathIndex.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOUR_PROJECT_API UCTask_IncrementPatrolPathIndex : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UCTask_IncrementPatrolPathIndex(FObjectInitializer const& ObjectInitializer);
+
+	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& Owner, uint8* NodeMemory) override;
+	
+};
