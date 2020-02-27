aeb96f8cf796600e3770210bba51396b5683f743
Ibby <ibrahimhussain2002@gmail.com>
2020-02-27 10:10:53 -0800

    AI
    Created new service to chnage ai speed
    
    decalred functions and variables

---
+++ Source/Parkour_Project/CService_ChangeSpeed.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CService_ChangeSpeed.h"
+
+void UCService_ChangeSpeed::OnBecomeRelevant(UBehaviorTreeComponent & Owner, uint8 * NodeMemory)
+{
+}
+
+FString UCService_ChangeSpeed::GetStaticServiceDescription() const
+{
+	return FString();
+}
---
+++ Source/Parkour_Project/CService_ChangeSpeed.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "CService_ChangeSpeed.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOUR_PROJECT_API UCService_ChangeSpeed : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+public:
+	UCService_ChangeSpeed();
+
+	void OnBecomeRelevant(UBehaviorTreeComponent& Owner, uint8* NodeMemory) override;
+
+protected:
+
+	FString GetStaticServiceDescription() const override;
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	float DefaultSpeed = 600.0f;
+	
+};
