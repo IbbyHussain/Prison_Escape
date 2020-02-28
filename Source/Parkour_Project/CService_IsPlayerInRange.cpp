5cd4f30fc0921cba3d8464f199eed8041b3f5c84
Ibby <ibrahimhussain2002@gmail.com>
2020-02-28 11:59:10 -0800

    AI Attack
    Calculates distance to player to see if the ai can attack

--- Source/Parkour_Project/CService_IsPlayerInRange.cpp
+++ Source/Parkour_Project/CService_IsPlayerInRange.cpp
@@ -5,7 +5,7 @@
 #include "C_SamuariAIController.h"
 #include "AI_Samurai.h"
 #include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
-#include "GameFramework/Character.h"
+#include "Player_Character.h"
 #include "Runtime/Engine/Classes/Engine/World.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BlackBoardKeys.h"
@@ -22,4 +22,14 @@ UCService_IsPlayerInRange::UCService_IsPlayerInRange()
 void UCService_IsPlayerInRange::OnBecomeRelevant(UBehaviorTreeComponent & Owner, uint8 * NodeMemory)
 {
 	Super::OnBecomeRelevant(Owner, NodeMemory);
+
+	//Get AI Controller Reference
+	AC_SamuariAIController* const Controller = Cast<AC_SamuariAIController>(Owner.GetAIOwner);
+
+	AAI_Samurai* const AISamuraiReference = Cast<AAI_Samurai>(Controller->GetPawn());
+
+	//Get player character reference
+	ACharacter* const PlayerCharacterReference = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	Controller->get_blackboard()->SetValueAsBool(bb_Keys::bPlayerIsInMeleeRange, AISamuraiReference->GetDistanceTo(PlayerCharacterReference) <= MeleeRange);
 }
