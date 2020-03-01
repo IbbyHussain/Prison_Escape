0393b01dce9af1ffecd2b54bc881a2e1414a9f43
Ibby <ibrahimhussain2002@gmail.com>
2020-03-01 08:57:20 -0800

    AI Weapon
    created overlap event
    
    ref for player
    
    checks if overlapped with player character

--- Source/Parkour_Project/AI_Samurai_Guard_Weapon.cpp
+++ Source/Parkour_Project/AI_Samurai_Guard_Weapon.cpp
@@ -27,7 +27,9 @@ AAI_Samurai_Guard_Weapon::AAI_Samurai_Guard_Weapon()
 void AAI_Samurai_Guard_Weapon::BeginPlay()
 {
 	Super::BeginPlay();
-	//SetActorRelativeScale3D(FVector(1.0f, 0, 1.f));
+	
+	// overlap 
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AAI_Samurai_Guard_Weapon::OnOverlapBegin);
 
 	TSubclassOf<AAI_Samurai> ClassToFind;
 	ClassToFind = AAI_Samurai::StaticClass();
@@ -44,6 +46,17 @@ void AAI_Samurai_Guard_Weapon::test()
 	
 	//Weaponref->DetachFromActor();
 
+}
+
+//OVERLAP
+void AAI_Samurai_Guard_Weapon::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherActor == PlayerCharacterReference)
+	{
+		UE_LOG(LogTemp, Log, TEXT("OVERLAPPED"));
+	}
+
+	
 }
 
 // Called every frame
--- Source/Parkour_Project/AI_Samurai_Guard_Weapon.h
+++ Source/Parkour_Project/AI_Samurai_Guard_Weapon.h
@@ -9,6 +9,7 @@
 //Define external classes here:
 class UBoxComponent;
 class AAI_Samurai;
+class APlayer_Character;
 
 UCLASS()
 class PARKOUR_PROJECT_API AAI_Samurai_Guard_Weapon : public AActor
@@ -32,7 +33,10 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Collision")
 	UBoxComponent* BoxComponent;
 
-	
+	UFUNCTION(BlueprintCallable, Category = "OnOverlapBeginAK47PickUp")
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	APlayer_Character* PlayerCharacterReference;
 
 public:	
 	// Called every frame
