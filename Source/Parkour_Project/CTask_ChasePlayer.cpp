7ebb3c8eda89eae4487cec7c57a9dcbabda5ec63
Ibby <ibrahimhussain2002@gmail.com>
2020-03-01 07:05:36 -0800

    AI
    Re-Wrote the AI Movement

--- Source/Parkour_Project/CTask_ChasePlayer.cpp
+++ Source/Parkour_Project/CTask_ChasePlayer.cpp
@@ -7,6 +7,11 @@
 #include "Blueprint/AIBlueprintHelperLibrary.h"
 #include "BlackBoardKeys.h"
 
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
+#include "Player_Character.h"
+
 UCTask_ChasePlayer::UCTask_ChasePlayer(FObjectInitializer const& object_initializer)
 {
 	NodeName = TEXT("Chase Player");
@@ -15,11 +20,32 @@ UCTask_ChasePlayer::UCTask_ChasePlayer(FObjectInitializer const& object_initiali
 EBTNodeResult::Type UCTask_ChasePlayer::ExecuteTask(UBehaviorTreeComponent & Owner, uint8 * node_memory)
 {
 	//Get target location
-	AC_SamuariAIController* const Controller = Cast<AC_SamuariAIController>(Owner.GetAIOwner());
-	FVector const PlayerLocation = Controller->get_blackboard()->GetValueAsVector(bb_Keys::TargetLocation);
+	/*AC_SamuariAIController* const Controller = Cast<AC_SamuariAIController>(Owner.GetAIOwner());
+	FVector const PlayerLocation = Controller->get_blackboard()->GetValueAsVector(bb_Keys::TargetLocation);*/
 
 	// Move to players location
-	UAIBlueprintHelperLibrary::SimpleMoveToLocation(Controller, PlayerLocation);
+	//UAIBlueprintHelperLibrary::SimpleMoveToLocation(Controller, PlayerLocation);
+
+	// Complex move to players location
+	//Reference to AI Controller
+	AC_SamuariAIController* const Controller = Cast<AC_SamuariAIController>(Owner.GetAIOwner());
+	
+	//Get Reference to the Player Character
+	APlayer_Character * PlayerCharacterReference = Cast<APlayer_Character>(Owner.GetBlackboardComponent()->GetValue<UBlackboardKeyType_Object>(bb_Keys::PlayerRef));
+
+	// If the player character is not null
+	if (PlayerCharacterReference)
+	{
+		// moves  the AI directly to the player character
+		Controller->MoveToActor(PlayerCharacterReference, 50.0f, true, true, true, 0, true);
+		
+
+		FinishLatentTask(Owner, EBTNodeResult::Succeeded);
+		return EBTNodeResult::Succeeded;
+	}
+	
+	/*FinishLatentTask(Owner, EBTNodeResult::Succeeded);
+	return EBTNodeResult::Succeeded;*/
 
 	FinishLatentTask(Owner, EBTNodeResult::Succeeded);
 	return EBTNodeResult::Succeeded;
