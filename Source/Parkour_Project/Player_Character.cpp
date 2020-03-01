66233f3843d5a24a9393e11c0a783fea4442e932
Ibby <ibrahimhussain2002@gmail.com>
2020-03-01 09:45:32 -0800

    AI Attack
    added coolision event in player class

--- Source/Parkour_Project/Player_Character.cpp
+++ Source/Parkour_Project/Player_Character.cpp
@@ -179,6 +179,10 @@ void APlayer_Character::SpawnKabuto(TSubclassOf<AC_Kabuto> KabutoClass)
 
 }
 
+void APlayer_Character::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+}
+
 // EVENT TICK-> Called every frame
 void APlayer_Character::Tick(float DeltaTime)
 {
--- Source/Parkour_Project/Player_Character.h
+++ Source/Parkour_Project/Player_Character.h
@@ -136,6 +136,16 @@ protected:
 
 	//KABUTO ATTACH
 	FName KabutoAttachSocketName;
+
+	//AIDAMAGE
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//AIDAMAGE
+	UPROPERTY(VisibleAnywhere, Category = "Trigger Capsule")
+	class UCapsuleComponent* TriggerCapsule;
+
+
 	
 public:	
 	// Called every frame
